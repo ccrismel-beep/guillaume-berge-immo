3e208ccd8b1b021d9afd5de7f30369fc06b862a4
ccrismel-beep <ccrismel@gmail.com>
2026-03-30 13:03:15 +0200

    Correction Caudéran

--- seo_web.py
+++ seo_web.py
@@ -217,7 +217,7 @@ def generer_html():
         <span class="tag">Gironde</span>
         <span class="tag">Nouvelle-Aquitaine</span>
         <span class="tag">Bruges</span>
-        <span class="tag">Caudéan</span>
+        <span class="tag">Caudéran</span>
       </div>
     </div>
 
